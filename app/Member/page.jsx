b171302787261ebbab0614392b85a14952887f4c
disciplineddev <disciplineddev1994@gmail.com>
2024-05-06 10:41:17 +0530

    first commit

---
+++ app/Member/page.jsx
@@ -0,0 +1,9 @@
+const Member = () => {
+  return (
+    <div>
+      <h1>Member server Session</h1>
+    </div>
+  );
+};
+
+export default Member;
---
+++ app/Public/page.jsx
@@ -0,0 +1,9 @@
+const Public = () => {
+  return (
+    <div>
+      <h1>This is Public Page</h1>
+    </div>
+  );
+};
+
+export default Public;
